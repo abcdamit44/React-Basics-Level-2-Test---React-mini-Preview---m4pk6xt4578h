04d032069e68b105d72e5a5bf3e0b7310283369d
Amit Singh <abcdamit44@gmail.com>
2022-04-15 13:24:25 +0530

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 import { Preview } from './Preview';
 const App = () => {
 
+  const [text, setText] = useState("hello world");
+  const [padding, setPadding] = useState(5);
+  const [fontSize, setFontSize] = useState(10);
   return (
     <div id="main">
       <div>
         {/* add input elememnts inside here */}
+        <label htmlFor="">Content</label><input type="text" id="contentInput" value={text} onChange={(e) => { setText(e.target.value) }} /> <br />
+        <label htmlFor="">Padding</label><input type="number" id="paddingInput" value={padding} onChange={(e) => { setPadding(e.target.value) }} /> <br />
+        <label htmlFor="">Font Size</label><input type="number" id="fontSizeInput" value={fontSize} onChange={(e) => { setFontSize(e.target.value) }} />
       </div>
- 
- {/* render Preview component here */}
- 
+      {/* render Preview component here */}
+      {/* <h1>{text.content}</h1> */}
+      <Preview fontSize={fontSize} content={text} padding={padding} />
     </div>
   )
 }
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const Preview = ({fontSize,content,padding}) =>{
+export const Preview = ({ fontSize, content, padding }) => {
     return (
-        <div id="preview" style={{padding,fontSize}}>
+        <div id="preview" style={{ padding: `${padding}px`, fontSize: `${fontSize}px` }}>
             {content}
         </div>
     )
